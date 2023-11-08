1c732cb90a039bbba200ac889740d70b0f84b57e
btlenny <btlenny@gmail.com>
2023-11-07 16:19:36 -0800

    trying svg

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,12 +5,13 @@ from . import views
 
 urlpatterns = [
     # 	RECIPE FUNCTIONS
-	  path('', views.home, name = 'home'),
+	path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
     path('recipes/', views.recipes_index, name = 'index'),
     path('recipes/<int:recipe_id>/', views.recipes_detail, name = 'detail'),
     path('recipes/create/', views.RecipeCreate.as_view(), name = 'recipes_create'),
     path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name = 'recipes_update'),
+    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipes_delete'),
     # 	DISH_TYPE FUNCTIONS
 	  path('dish_types/', views.Dish_TypeList.as_view(), name = 'dish_types_index'),
     path('dish_types/<int:pk>/', views.Dish_TypeDetail.as_view(), name = 'dish_types_detail'),
